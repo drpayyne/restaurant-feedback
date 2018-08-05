c46b05d1083de63030562aaad0a19ac6c97aa4e1
Adarsh M <adarsh.apple@icloud.com>
2018-08-05 20:38:10 +0530

    animating gradient background, switch added

--- app/src/main/java/com/lazytomatostudios/feedback/MainActivity.java
+++ app/src/main/java/com/lazytomatostudios/feedback/MainActivity.java
@@ -1,13 +1,39 @@
 package com.lazytomatostudios.feedback;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout container;
+    AnimationDrawable animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        container = findViewById(R.id.container);
+
+        animation = (AnimationDrawable) container.getBackground();
+        animation.setEnterFadeDuration(500);
+        animation.setExitFadeDuration(1500);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animation != null && !animation.isRunning())
+            animation.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animation != null && animation.isRunning())
+            animation.stop();
     }
 }
